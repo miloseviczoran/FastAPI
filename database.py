c7e662ef80b3bc48232d637bc032f30624ebf915
Zoran <mag.milosevic.zoran@gmail.com>
2025-03-09 15:54:49 +0100

    Production database change

--- database.py
+++ database.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost/TodoApplicationDatabase"
+SQLALCHEMY_DATABASE_URL = "postgresql://deploy_database_postgresql_user:yQduYz1qaQE1jrcGthOOdt98SIgCqniB@dpg-cv6qjta3esus73e8vepg-a/deploy_database_postgresql"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
